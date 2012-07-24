dd420566939195f2778654218c4520b93bbfcfb4
Matthew Tretter <matthew@exanimo.com>
2012-07-24 15:11:18 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
 import os
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-README = read('README.markdown')
 
 setup(
-    name = 'django-wsgitools',
-    version = '0.1.1',
+    name='django-wsgitools',
+    version='0.1.1',
     description='Tools for using Django + mod_wsgi.',
-    long_description=README,
-
-    author = 'Matthew Tretter',
-    author_email = 'matthew@exanimo.com',
-    packages = [
-        'wsgitools',
+    long_description=read('README.markdown'),
+    author='Matthew Tretter',
+    author_email='matthew@exanimo.com',
+    packages=find_packages(),
+    install_requires=[
     ],
-    install_requires = [
-    ],
-    classifiers = [
+    classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
